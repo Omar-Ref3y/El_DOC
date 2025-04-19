725e0d5eaee9718400946e9ee32e3b677e9d24b5
Omar Refay <omarrefay2004@gmail.com>
2025-04-19 23:21:24 +0200

    This is the Begining of my first MVC project

---
+++ Presentation_Layer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace El_DOC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
